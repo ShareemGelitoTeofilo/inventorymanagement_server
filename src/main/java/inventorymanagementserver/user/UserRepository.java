b44c1be6f5ee216db80a10df334b7e6c4a6e722b
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-19 13:35:16 +0800

    Created UserRepository

---
+++ src/main/java/inventorymanagementserver/user/UserRepository.java
@@ -0,0 +1,7 @@
+package inventorymanagementserver.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsernameAndPassword(String username, String password);
+}
