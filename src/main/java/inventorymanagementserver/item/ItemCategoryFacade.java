5b05fb128a7beb107e6ec667cf427ec4b6af8fa9
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-19 18:13:04 +0800

    added @Component Spring boot annotation

--- src/main/java/inventorymanagementserver/item/ItemCategoryFacade.java
+++ src/main/java/inventorymanagementserver/item/ItemCategoryFacade.java
@@ -3,7 +3,9 @@ package inventorymanagementserver.item;
 import inventorymanagementserver.category.Category;
 import inventorymanagementserver.category.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 class ItemCategoryFacade {
 
     @Autowired
