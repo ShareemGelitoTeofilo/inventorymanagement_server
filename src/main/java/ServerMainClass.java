0ade482f74c1c2526d215366ba34ee62b244f465
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-19 12:54:31 +0800

    Committing all files

---
+++ src/main/java/ServerMainClass.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerMainClass.class, args);
+    }
+}
