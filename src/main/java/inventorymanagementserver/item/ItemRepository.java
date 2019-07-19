302dabb46a802f05a0605382c2cb898a12274dfb
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-19 17:05:31 +0800

    added method for finding item by name

--- src/main/java/inventorymanagementserver/item/ItemRepository.java
+++ src/main/java/inventorymanagementserver/item/ItemRepository.java
@@ -3,4 +3,5 @@ package inventorymanagementserver.item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    Item findByName(String name);
 }
