2edcf5fcd49666d44b55a2722f6f9b3cec95ff52
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-19 17:30:11 +0800

    create implementation for finding item by id and name and finding all

--- src/main/java/inventorymanagementserver/item/ItemService.java
+++ src/main/java/inventorymanagementserver/item/ItemService.java
@@ -1,5 +1,10 @@
 package inventorymanagementserver.item;
 
+import java.util.List;
+
 public interface ItemService {
     Item registerItem(Item item) throws Exception;
+    Item findById(Long id) throws Exception;
+    Item findByName(String name) throws Exception;
+    List<Item> findAll() throws Exception;
 }
--- src/main/java/inventorymanagementserver/item/ItemServiceImpl.java
+++ src/main/java/inventorymanagementserver/item/ItemServiceImpl.java
@@ -3,6 +3,8 @@ package inventorymanagementserver.item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ItemServiceImpl implements ItemService {
 
@@ -16,5 +18,31 @@ public class ItemServiceImpl implements ItemService {
             throw new Exception("Item with the a same name already exist");
         }
         return itemRepository.save(item);
+    }
+
+    @Override
+    public Item findById(Long id) throws Exception {
+        String message = String.format("Item with the ID %s not found", id);
+        return itemRepository.findById(id)
+                .orElseThrow(()-> new Exception(message));
+    }
+
+    @Override
+    public Item findByName(String name) throws Exception {
+        Item item = itemRepository.findByName(name);
+        if(item == null){
+            String message = String.format("Item with name %s not found", name);
+            throw new Exception(message);
+        }
+        return item;
+    }
+
+    @Override
+    public List<Item> findAll() throws Exception {
+        List<Item> items = itemRepository.findAll();
+        if(items == null || items.isEmpty()){
+            throw new Exception("No item/s found");
+        }
+        return items;
     }
 }
