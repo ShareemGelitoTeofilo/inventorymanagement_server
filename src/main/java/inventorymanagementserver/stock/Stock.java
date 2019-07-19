76584eecad7a8aa5072938f950de98c5fbdb7739
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-19 13:46:10 +0800

    Added one to one relationship annotation with item

--- src/main/java/inventorymanagementserver/stock/Stock.java
+++ src/main/java/inventorymanagementserver/stock/Stock.java
@@ -5,10 +5,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -19,6 +16,7 @@ public class Stock {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne
     private Item item;
     private int quantity;
 
