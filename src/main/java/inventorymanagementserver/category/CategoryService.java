9d6d61bee493b714871bef53e3b6a7d604bc4e5c
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-19 15:44:02 +0800

    create method for finding category by id

--- src/main/java/inventorymanagementserver/category/CategoryService.java
+++ src/main/java/inventorymanagementserver/category/CategoryService.java
@@ -1,6 +1,9 @@
 package inventorymanagementserver.category;
 
+import java.util.List;
+
 public interface CategoryService {
     Category registerCategory(Category category) throws Exception;
     Category findById(Long id) throws Exception;
+    List<Category> findAll() throws Exception;
 }
