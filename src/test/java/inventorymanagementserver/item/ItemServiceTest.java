91df27f8b3d6a0a4cdbb4457fe396112ee6f4bb4
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-19 17:29:50 +0800

    create test for finding item by id and name and finding all

--- src/test/java/inventorymanagementserver/item/ItemServiceTest.java
+++ src/test/java/inventorymanagementserver/item/ItemServiceTest.java
@@ -6,14 +6,18 @@ import inventorymanagementserver.category.CategoryRepository;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.UUID;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 public class ItemServiceTest extends ServerMainClassTest {
 
     @Autowired
     ItemService itemService;
+    @Autowired
+    ItemRepository itemRepository;
     @Autowired
     private CategoryRepository categoryRepository;
 
@@ -23,12 +27,40 @@ public class ItemServiceTest extends ServerMainClassTest {
         assertNotNull(item);
     }
 
+    @Test
+    public void findItemById() throws Exception {
+        Long id = createSavedItem().getId();
+        Item item = itemService.findById(id);
+        assertNotNull(item);
+    }
+
+    @Test
+    public void findItemByName() throws Exception {
+        String name = createSavedItem().getName();
+        Item item = itemService.findByName(name);
+        assertNotNull(item);
+    }
+
+    @Test
+    public void findAllItem() throws Exception {
+        List<Item> items = itemService.findAll();
+        assertFalse(items.isEmpty());
+    }
+
     private Item createItem() {
         String name = UUID.randomUUID().toString();
         return new Item(
                 name, "brand", "color",
                 createCategory(), 123.00
         );
+    }
+
+    private Item createSavedItem() {
+        String name = UUID.randomUUID().toString();
+        return itemRepository.save(new Item(
+                name, "brand", "color",
+                createCategory(), 123.00
+        ));
     }
 
     private Category createCategory() {
