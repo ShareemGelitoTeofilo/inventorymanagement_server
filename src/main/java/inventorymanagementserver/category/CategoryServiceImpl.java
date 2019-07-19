c93221560888d82aff3e0431aa962540d002d6de
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-19 15:41:04 +0800

    create implementation for finding category by id

--- src/main/java/inventorymanagementserver/category/CategoryServiceImpl.java
+++ src/main/java/inventorymanagementserver/category/CategoryServiceImpl.java
@@ -10,7 +10,18 @@ public class CategoryServiceImpl implements CategoryService {
     CategoryRepository categoryRepository;
 
     @Override
-    public Category registerCategory(Category category) {
+    public Category registerCategory(Category category) throws Exception {
+        Category existingCategory = categoryRepository.findByName(category.getName());
+        if(existingCategory != null){
+            throw new Exception("Category already exist");
+        }
         return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category findById(Long id) throws Exception {
+        String message = String.format("Category with ID %s not found", id);
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new Exception(message));
     }
 }
