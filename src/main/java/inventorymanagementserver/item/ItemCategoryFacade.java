d3477fb75e45902a35c2a7c3d942eafa7a9ce0cd
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-19 18:06:09 +0800

    created ItemCategoryFacade

---
+++ src/main/java/inventorymanagementserver/item/ItemCategoryFacade.java
@@ -0,0 +1,16 @@
+package inventorymanagementserver.item;
+
+import inventorymanagementserver.category.Category;
+import inventorymanagementserver.category.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class ItemCategoryFacade {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public void checkIfCategoryExist(Category category) throws Exception {
+        Category existingCategory = categoryRepository.findById(category.getId())
+                .orElseThrow(()-> new Exception("Category no longer exist"));
+    }
+}
