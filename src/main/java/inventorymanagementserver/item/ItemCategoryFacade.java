7648b1a2e577adc8e8dd7f23a7622ba037341135
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-20 18:55:38 +0800

    modified class to be public

--- src/main/java/inventorymanagementserver/item/ItemCategoryFacade.java
+++ src/main/java/inventorymanagementserver/item/ItemCategoryFacade.java
@@ -5,14 +5,23 @@ import inventorymanagementserver.category.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-class ItemCategoryFacade {
+public class ItemCategoryFacade {
 
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ItemRepository itemRepository;
 
     public void checkIfCategoryExist(Category category) throws Exception {
         Category existingCategory = categoryRepository.findById(category.getId())
                 .orElseThrow(()-> new Exception("Category no longer exist"));
+    }
+
+    public boolean checkIfCategoryHasRelationship(Category category) {
+       List<Item> itemsUsingTheCategory = itemRepository.findAllByCategory(category);
+       return !itemsUsingTheCategory.isEmpty();
     }
 }
