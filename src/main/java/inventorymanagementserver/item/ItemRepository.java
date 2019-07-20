447ff05c5f5f7a7c6fba033cf03681453eb6929e
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-20 18:49:12 +0800

    add a method for finding an item by category

--- src/main/java/inventorymanagementserver/item/ItemRepository.java
+++ src/main/java/inventorymanagementserver/item/ItemRepository.java
@@ -1,7 +1,11 @@
 package inventorymanagementserver.item;
 
+import inventorymanagementserver.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Item findByName(String name);
+    List<Item> findAllByCategory(Category category);
 }
