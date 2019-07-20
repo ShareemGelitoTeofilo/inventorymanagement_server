64a111681443a1788dd7d86c48149bf2756866d0
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-20 19:49:25 +0800

    Created methods to update and find all stocks

--- src/main/java/inventorymanagementserver/stock/StockService.java
+++ src/main/java/inventorymanagementserver/stock/StockService.java
@@ -1,7 +1,11 @@
 package inventorymanagementserver.stock;
 
+import java.util.List;
+
 public interface StockService {
     Stock insert(Stock stock);
     Stock findById(Long id) throws Exception;
     Stock findByItemId(Long id) throws Exception;
+    List<Stock> findAll() throws Exception;
+    Stock update(Stock stock) throws Exception;
 }
