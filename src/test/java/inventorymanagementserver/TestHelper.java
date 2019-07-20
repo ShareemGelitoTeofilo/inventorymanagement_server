a0c489fc8aef6c4fdb12f373ac4f38c44280059b
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-20 19:26:28 +0800

    Created a method create and save stock

--- src/test/java/inventorymanagementserver/TestHelper.java
+++ src/test/java/inventorymanagementserver/TestHelper.java
@@ -4,6 +4,8 @@ import inventorymanagementserver.category.Category;
 import inventorymanagementserver.category.CategoryRepository;
 import inventorymanagementserver.item.Item;
 import inventorymanagementserver.item.ItemRepository;
+import inventorymanagementserver.stock.Stock;
+import inventorymanagementserver.stock.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,18 +18,46 @@ public class TestHelper {
     private ItemRepository itemRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private StockRepository stockRepository;
 
+
+    public Item createItem() {
+        String name = UUID.randomUUID().toString();
+        return new Item(
+                name, "brand", "color",
+                createSavedCategory(), 123.00
+        );
+    }
 
     public Item createSavedItem() {
         String name = UUID.randomUUID().toString();
         return itemRepository.save(new Item(
                 name, "brand", "color",
-                createCategory(), 123.00
+                createSavedCategory(), 123.00
         ));
     }
 
     public Category createCategory() {
+        return new Category("name");
+    }
+
+    public Category createSavedCategory() {
         Category category = new Category("name");
         return categoryRepository.save(category);
+    }
+
+    private Stock createStock() {
+        Item item = createSavedItem();
+        int quantity = 12;
+        return new Stock(item, quantity);
+    }
+
+    private Stock createSavedStock() {
+        Item item = createSavedItem();
+        int quantity = 12;
+        return stockRepository.save(
+                new Stock(item, quantity)
+        );
     }
 }
