de7b620f1393fda361ccd66883bdd78c19cebc5d
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-20 22:06:28 +0800

    created a method for finding all users, updating and deleting a user

--- src/main/java/inventorymanagementserver/user/UserService.java
+++ src/main/java/inventorymanagementserver/user/UserService.java
@@ -1,5 +1,13 @@
 package inventorymanagementserver.user;
 
+import java.util.List;
+
 public interface UserService {
     User signUpUser(User user) throws Exception;
+    User login(String username, String password) throws Exception;
+    User findById(Long id) throws Exception;
+    User findByUsername(String username) throws Exception;
+    List<User> findAll();
+    User update(User user) throws Exception;
+    void deleteById(Long id) throws Exception;
 }
