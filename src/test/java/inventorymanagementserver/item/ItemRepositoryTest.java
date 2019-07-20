56a339fc2a65006314e99f16bd72fc41e4a32342
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-20 17:49:12 +0800

    Refactored to use TestHelper class

--- src/test/java/inventorymanagementserver/item/ItemRepositoryTest.java
+++ src/test/java/inventorymanagementserver/item/ItemRepositoryTest.java
@@ -1,13 +1,11 @@
 package inventorymanagementserver.item;
 
 import inventorymanagementserver.ServerMainClassTest;
-import inventorymanagementserver.category.Category;
-import inventorymanagementserver.category.CategoryRepository;
+import inventorymanagementserver.TestHelper;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.UUID;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -18,25 +16,25 @@ public class ItemRepositoryTest extends ServerMainClassTest {
     @Autowired
     ItemRepository itemRepository;
     @Autowired
-    CategoryRepository categoryRepository;
+    TestHelper testHelper;
 
     @Test
     public void saveItem(){
-        Item item = createItem();
+        Item item = testHelper.createItem();
         item = itemRepository.save(item);
         assertNotNull(item);
     }
 
     @Test
     public void findItemById(){
-        Item item = createSavedItem();
+        Item item = testHelper.createSavedItem();
         item = itemRepository.findById(item.getId()).get();
         assertNotNull(item);
     }
 
     @Test
     public void findItemByName(){
-        Item item = createSavedItem();
+        Item item = testHelper.createSavedItem();
         item = itemRepository.findByName(item.getName());
         assertNotNull(item);
     }
@@ -49,11 +47,11 @@ public class ItemRepositoryTest extends ServerMainClassTest {
 
     @Test
     public void updateItem(){
-        Item item = createSavedItem();
+        Item item = testHelper.createSavedItem();
         item.setName("new name");
         item.setBrand("new brand");
         item.setColor("new color");
-        item.setCategory(createCategory());
+        item.setCategory(testHelper.createCategory());
         item.setPrice(123.23);
         item = itemRepository.save(item);
         assertNotNull(item);
@@ -61,31 +59,8 @@ public class ItemRepositoryTest extends ServerMainClassTest {
 
     @Test
     public void deleteItemById(){
-        Item item = createSavedItem();
+        Item item = testHelper.createSavedItem();
         itemRepository.delete(item);
-    }
-
-
-    private Item createItem() {
-        String name = UUID.randomUUID().toString();
-        return new Item(
-                name, "brand", "color",
-                createCategory(), 123.00
-        );
-    }
-
-    private Item createSavedItem() {
-        String name = UUID.randomUUID().toString();
-        return itemRepository.save(new Item(
-                name, "brand", "color",
-                createCategory(), 123.00
-        ));
-    }
-
-
-    private Category createCategory() {
-        Category category = new Category("name");
-        return categoryRepository.save(category);
     }
 
 }
