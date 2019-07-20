bac44ba1424fe6ec1ee8c66252847cc76e0943d2
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-20 19:35:01 +0800

    Created methods to find stock by id and item id

--- src/main/java/inventorymanagementserver/stock/StockService.java
+++ src/main/java/inventorymanagementserver/stock/StockService.java
@@ -2,4 +2,6 @@ package inventorymanagementserver.stock;
 
 public interface StockService {
     Stock insert(Stock stock);
+    Stock findById(Long id) throws Exception;
+    Stock findByItemId(Long id) throws Exception;
 }
