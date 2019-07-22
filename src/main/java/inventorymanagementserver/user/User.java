231c11b514a021d13f32a9d8b79854e5e0240858
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-22 11:39:56 +0800

    Add type to user

--- src/main/java/inventorymanagementserver/user/User.java
+++ src/main/java/inventorymanagementserver/user/User.java
@@ -19,13 +19,15 @@ public class User {
     private String address;
     private String username;
     private String password;
+    private UserType type;
 
     protected User() {}
 
-    public User(String name, String address, String username, String password) {
+    public User(String name, String address, String username, String password, UserType type) {
         this.name = name;
         this.address = address;
         this.username = username;
         this.password = password;
+        this.type = type;
     }
 }
