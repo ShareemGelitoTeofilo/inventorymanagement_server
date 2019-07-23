330335e110d88358b6de0227ceadccba33d14f89
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-23 15:04:19 +0800

    removed all exception throws

--- src/main/java/inventorymanagementserver/user/UserService.java
+++ src/main/java/inventorymanagementserver/user/UserService.java
@@ -3,11 +3,11 @@ package inventorymanagementserver.user;
 import java.util.List;
 
 public interface UserService {
-    User signUpUser(User user) throws Exception;
-    User login(String username, String password) throws Exception;
-    User findById(Long id) throws Exception;
-    User findByUsername(String username) throws Exception;
+    User signUp(User user);
+    User login(String username, String password);
+    User findById(Long id);
+    User findByUsername(String username);
     List<User> findAll();
-    User update(User user) throws Exception;
-    void deleteById(Long id) throws Exception;
+    User update(User user);
+    void deleteById(Long id);
 }
