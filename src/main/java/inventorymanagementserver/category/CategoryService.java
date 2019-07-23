b58d8b2c9918d56feb868777847de57a429423d6
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-23 13:14:59 +0800

    renamed method

--- src/main/java/inventorymanagementserver/category/CategoryService.java
+++ src/main/java/inventorymanagementserver/category/CategoryService.java
@@ -3,9 +3,9 @@ package inventorymanagementserver.category;
 import java.util.List;
 
 public interface CategoryService {
-    Category registerCategory(Category category) throws Exception;
-    Category findById(Long id) throws Exception;
-    List<Category> findAll() throws Exception;
-    Category updateCategory(Category category) throws Exception;
-    void deleteById(Long id) throws Exception;
+    Category insert(Category category);
+    Category findById(Long id);
+    List<Category> findAll();
+    Category update(Category category);
+    void deleteById(Long id);
 }
