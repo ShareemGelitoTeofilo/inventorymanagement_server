adf0802e69355c5b06e7ac33de8df21701a5e28b
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-23 12:57:40 +0800

    refactored implementation

--- src/main/java/inventorymanagementserver/category/CategoryServiceImpl.java
+++ src/main/java/inventorymanagementserver/category/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package inventorymanagementserver.category;
 
-import inventorymanagementserver.item.ItemCategoryFacade;
+import inventorymanagementserver.exception.InventoryException;
+import inventorymanagementserver.item.Item;
+import inventorymanagementserver.item.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,51 +12,58 @@ import java.util.List;
 public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
-    CategoryRepository categoryRepository;
+    private CategoryRepository categoryRepository;
     @Autowired
-    ItemCategoryFacade itemCategoryFacade;
+    private ItemRepository itemRepository;
 
     @Override
-    public Category registerCategory(Category category) throws Exception {
+    public Category registerCategory(Category category) {
         Category existingCategory = categoryRepository.findByName(category.getName());
         if(existingCategory != null){
-            throw new Exception("Category already exist");
+            throw new InventoryException("Category already exist");
         }
         return categoryRepository.save(category);
     }
 
     @Override
-    public Category findById(Long id) throws Exception {
+    public Category findById(Long id) {
         String message = String.format("Category with ID %s not found", id);
         return categoryRepository.findById(id)
-                .orElseThrow(() -> new Exception(message));
+                .orElseThrow(() -> new InventoryException(message));
     }
 
     @Override
-    public List<Category> findAll() throws Exception {
+    public List<Category> findAll()  {
         List<Category> categories = categoryRepository.findAll();
-        if(categories == null || categories.isEmpty()){
-            throw new Exception("No category found");
+        if(categories.isEmpty()){
+            throw new InventoryException("No category found");
         }
         return categories;
     }
 
     @Override
-    public Category updateCategory(Category category) throws Exception {
-        findById(category.getId());
+    public Category updateCategory(Category category) {
+        if (!categoryRepository.existsById(category.getId()))    {
+            throw new InventoryException("Category doesn't exist");
+        }
         Category existingCategoryWithSameName = categoryRepository.findByName(category.getName());
         if(existingCategoryWithSameName != null && !existingCategoryWithSameName.equals(category)){
-            throw new Exception("Category name already taken");
+            throw new InventoryException("Category name already taken");
         }
         return categoryRepository.save(category);
     }
 
     @Override
-    public void deleteById(Long id) throws Exception {
+    public void deleteById(Long id) {
         Category category = findById(id);
-        if(itemCategoryFacade.checkIfCategoryHasRelationship(category)){
-            throw new Exception("This category is used by an item");
+        if (checkIfCategoryHasRelationship(category)) {
+            throw new InventoryException("This category is used by an item");
         }
-        categoryRepository.deleteById(id);
+        categoryRepository.deleteById(category.getId());
+    }
+
+    private boolean checkIfCategoryHasRelationship(Category category) {
+        List<Item> itemsUsingTheCategory = itemRepository.findAllByCategory(category);
+        return !itemsUsingTheCategory.isEmpty();
     }
 }
